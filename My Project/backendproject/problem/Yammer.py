375792fdac2d35d045ea06acf2395213b6158c94
RohitPatil0045 <rohitpatil0036@gmail.com>
2022-02-23 18:48:48 +0530

    23 feb 2022

--- My Project/backendproject/problem/Yammer.py
+++ My Project/backendproject/problem/Yammer.py
@@ -57,13 +57,81 @@
 # print(twoSum( [2, 5, 6, 7], 8));
 
 
-a = ['a', 'b', 'v', 'g']
+# a = ['a', 'b', 'v', 'g']
 # a[:]= a[-1:-5]
 # print(a)
 # s.reverse()
 
-for i in range(len(a)):
-    for j in range(i + 1, len(a)):
-        print ([i,j])
+# for i in range(len(a)):
+#     for j in range(i + 1, len(a)):
+#         print ([i,j])
     # for k in range(len(a)):
     #     print([i,k])
+
+
+# l1 = [2,4,3]
+# l2 = [5,6,4]
+#
+# l1.reverse()
+# l2.reverse()
+# a = ''
+# b = ''
+# for i in l1 :
+#     a += str(i)
+#
+# for i in l2 :
+#     b += str(i)
+#
+# add = int(a) + int(b)
+#
+#
+# output = []
+# for i in str(add) :
+#     output.append(int(i))
+#
+# output.reverse()
+#
+# # print (output)
+#
+#
+# l1 = [2,4,3]
+# a[:] : l1[::-1]
+# print (a)
+# l2 = [5,6,4]
+# b[:] : l2[::-1]
+# add = int(a) + int(b)
+# print(add)
+# output = []
+# for i in str(add) :
+#     output.append(int(i))
+#
+# print(output[::-1])
+
+
+# c[:]:output[::-1]
+# print (c)
+
+
+s = "babad"
+
+for i in range(len(s)):
+    if s[i] ==
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
